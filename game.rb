c4a37d9da40bb10f6dea15166bee73812f64cac5
Robin Bisho <robinbisho@gmail.com>
2017-07-08 23:06:17 -0700

    Initial grabbing code.

--- game.rb
+++ game.rb
@@ -1,13 +1,25 @@
-class Actor
-  attr_reader :name
-  attr_accessor :hp
+class Entity
+  attr_reader :name, :room
+  def initialize(opts)
+    @room = opts[:room] || TestRoom.new
+    @name = opts[:name] || "[NAME NOT SET]"
+    @room.entity_list.push(self)
+  end
+
+  def grabbed(junk)
+    puts "You cannot grab the #{@name}."
+  end
+end
+
+class Actor < Entity
+  attr_accessor :hp, :right_hand, :level, :race
 
   def initialize(opts = {})
-    @name = opts[:name] || "[NAME NOT SET]"
-    @room = opts[:room]
-    @room.push(self)
+    super(opts)
+    @level = 0
+    @race = "RACE NOT SET"
     @hp = 10
-    @right_hand = Sword.new
+    @right_hand = nil
   end
 
   def targets
@@ -15,9 +27,24 @@ class Actor
     output.delete(self)
     output
   end
+
+  def grab(target)
+    target.grabbed(self)
+  end
+
+  def look_text
+    "You are a level #{@level} #{@race}.
+    test."
+  end
 end
 
 class Player < Actor
+  def initialize(opts)
+    super(opts)
+    @level = 1
+    @race = "human"
+  end
+
   def attack(target)
     prev_hp = target.hp
     @right_hand.attack(target)
@@ -33,29 +60,37 @@ class Enemy < Actor
   end
 end
 
-class Sword
-  attr_reader :damage
-
-  def initialize
+class Sword < Entity
+  def initialize(opts)
+    super(opts)
+    @name = "sword"
     @damage = 1
   end
+
   def attack(target)
     target.hp -= @damage
+  end
+
+  def grabbed(grabber)
+    grabber.right_hand = self
+    puts "You grab the sword."
   end
 end
 
 class Main
   def initialize
-    @room = []
+    @room = TestRoom.new
     @player = Player.new(room: @room)
     @enemy = Enemy.new(room: @room, name: "enemy crab")
     @user = User.new(player: @player)
+    @sword = Sword.new(room: @room)
   end
 
   def run
     loop do
+      puts
       @user.parse
-      @enemy.attack(@player)
+      # @enemy.attack(@player)
     end
   end
 end
@@ -65,38 +100,46 @@ class User
     @player = opts[:player]
     @commands = {
       attack: :attack,
+      grab: :grab,
+      look: :look,
       quit: :halt
     }
   end
 
   def parse
     print " > "
-    command, target = gets.chomp.split(" ", 2)
-    run_command(command.to_sym, target)
+    command_name, target_name = gets.chomp.split(" ", 2)
+    command = find_command(command_name.to_sym)
+    if target_name != nil
+      target = find_target(target_name)
+    else
+      target = nil
+    end
+    if command == :bad_command
+      bad_command(command_name)
+    elsif target == :bad_target
+      bad_target(target_name)
+    else
+      send(command, target)
+    end
   end
 
-  def run_command(command, target)
-    if @commands[command]
-      send(@commands[command], target)
-    else
-      bad_command(command)
+  def find_command(command_name)
+    @commands[command_name] || :bad_command
+  end
+
+  def find_target(target_name)
+    @player.room.entity_list.each do |entity|
+      if entity.name == target_name
+        return entity
+      end
     end
+    :bad_target
   end
 
   def bad_command(command)
     puts "Unknown command: #{command}"
     parse
-  end
-
-  def search(list, target)
-    output = false
-    list.each do |entity|
-      if entity.name == target
-        output = entity
-        break
-      end
-    end
-    output
   end
 
   def attack(target)
@@ -112,8 +155,24 @@ class User
   def halt(target)
     exit
   end
+
+  def look(target)
+    target ||= @player.room
+    puts target.look_text
+  end
+
+  def grab(entity_name)
+    @player.grab(entity_name)
+  end
 end
 
-
+class TestRoom
+  attr_reader :look_text
+  attr_accessor :entity_list
+  def initialize
+    @look_text = File.read("TestRoom.txt")
+    @entity_list = []
+  end
+end
 
 Main.new.run
